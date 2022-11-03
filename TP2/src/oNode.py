df571c07dd3334211315cc23d59dcca7dca33ebc
Ana Henriques <pbapohenriques@hotmail.com>
2022-11-03 15:01:59 +0000

    graph implemented

--- TP2/src/oNode.py
+++ TP2/src/oNode.py
@@ -1,14 +1,22 @@
 import socket
 import threading
+import re
 
+class interface:
 
-def start():
+    def __init__(self, nodeName, ip):
+        self.nodeName = nodeName
+        self.ip = ip
 
-# Thread with Server
+class oNode:
 
-
-
-# Thread with Client
-
-
+    # oNode [ip1:ip2] [ip2:ip3] [ip3:ip4,ip5]
+    def constructGraph(command):
+        graph = {}
+        pairs = re.findall("(?:\[(.*?)\])",command) #['A:B', 'B:C', 'C:D,E']
+        for p in pairs:
+            ips = p.split(' : ') # 'A', 'B'
+            ips2 = ips[1].split(',')
+            graph[f'{ips[0]}'] = ips2
+        return graph
 
