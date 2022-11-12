98026f644a31617ceb97e3320ca89226d13d0faa
Ana Henriques <pbapohenriques@hotmail.com>
2022-11-12 17:57:40 +0000

    new graph implemented

--- TP2/src/dijkstra.py
+++ TP2/src/dijkstra.py
@@ -1,63 +1,74 @@
 import socket
 import threading
 import re
-import oNode
 import heapq as hq
 import math
 
-oNode = oNode()
+def populateGraph(vertices,neighbors):
+    graph = [[0 for column in range(vertices)] for row in range(vertices)]
+    for row in range(vertices): # row = 0, 1, 2 ou 3
+        for column in range(vertices): # column = 0, 1, 2 ou 3
+            for element in range(len(neighbors)):
+                if row == neighbors[element][0] and column == neighbors[element][1]:
+                    graph[row][column] = 1
+                    graph[column][row] = 1
+                    break
+    return graph
 
-# command = sys.argv[1:]
-graph = oNode.constructGraph('oNode [10.0.0.1 : 10.0.0.2 , 10.0.0.3] [10.0.0.2 : 10.0.0.3, 10.0.0.6] [10.0.0.3 :] [10.0.0.6 :]')
-# graph = {'10.0.0.1' : ['10.0.0.2' , '10.0.0.3'], '10.0.0.2' : ['10.0.0.3', '10.0.0.6'], '10.0.0.3 :[]', '10.0.0.6 :'[]}
 
-
-# Python program for Dijkstra's single
-# source shortest path algorithm. The program is
-# for adjacency matrix representation of the graph
 class Graph():
  
-    def __init__(self, vertices):
+    def __init__(self,vertices,neighbors):
         self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
-        
+        self.graph = populateGraph(vertices,neighbors)
+
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
  
-    def printSolution(self, dist):
-        print("Vertex \t Distance from Source")
-        for node in range(self.V):
-            print(node, "\t\t", dist[node])
-
-
-# Function that implements Dijkstra's single source
-# shortest path algorithm for a graph represented
-# using adjacency matrix representation
-def dijkstra(self, src):
-
-    dist = [1e7] * self.V
-    dist[src] = 0
-    sptSet = [False] * self.V
-
-    for cout in range(self.V):
-
-        # Pick the minimum distance vertex from
-        # the set of vertices not yet processed.
-        # u is always equal to src in first iteration
-        u = self.minDistance(dist, sptSet)
-
-        # Put the minimum distance vertex in the
+        # Initialize minimum distance for next node
+        min = 1e7
+ 
+        # Search not nearest vertex not in the
         # shortest path tree
-        sptSet[u] = True
-
-        # Update dist value of the adjacent vertices
-        # of the picked vertex only if the current
-        # distance is greater than new distance and
-        # the vertex in not in the shortest path tree
         for v in range(self.V):
-            if (self.graph[u][v] > 0 and
-                sptSet[v] == False and
-                dist[v] > dist[u] + self.graph[u][v]):
-                dist[v] = dist[u] + self.graph[u][v]
-
-    self.printSolution(dist)
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+ 
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+ 
+        dist = [1e7] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, sptSet)
+ 
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            sptSet[u] = True
+ 
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and
+                   sptSet[v] == False and
+                   dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist)
  
--- TP2/src/oNode.py
+++ TP2/src/oNode.py
@@ -1,33 +1,51 @@
 import socket
 import threading
 import re
-from .dijkstra import Graph
+from dijkstra import Graph
+
 
 class interface:
 
-    def __init__(self, ip, nodeName, requiredFiles,currentFiles):
-        self.nodeName = nodeName               # o nome do nodos
+    def __init__(self, ip, requiredFiles,currentFiles):
         self.ip = ip                           # ips das interfaces ligadas aos nodos 
         self.requiredFiles = requiredFiles     # lista de ficheiros que ele quer
         self.currentFiles = currentFiles       # lista de ficheiros que ele tem
 
+
+def getIndex(li,ip):
+    for index, x in enumerate(li):
+        if x.ip == ip:
+            return index
+    return -1
+
+
 class oNode:
 
-    # oNode [ip1:ip2] [ip2:ip3] [ip3:ip4,ip5]
+    # oNode [ip1:ip2] [ip2:ip3,ip4] [ip3:ip4,ip5]
     def constructGraph(command):
-        graph = Graph()
-        pairs = re.findall("(?:\[(.*?)\])",command) #['A:B', 'B:C', 'C:D,E']
-        i = 0
+        nodes = []
+        pairs = re.findall("(?:\[(.*?)\])",command) #['A:B,C', 'B:C', 'C:D,E']
+        neighbors_aux = []   # [ (indA,indB,distanceAB), (indA,indB,distanceAC), (ind,C,distanceBC) ]
         for p in pairs:
             ips = re.split(r'\s*:\s*', p) # 'C', 'D,E'
-            interf1 = interface(ips[0],f'node{i}',[],[])
+            interf1 = interface(ips[0],[],[])
+            if not any(obj.ip == ips[0] for obj in nodes):
+                nodes.append(interf1)
             ips2 = re.split(r'\s*,\s', ips[1]) # 'D', 'E'
-            neighbors = []
             for next in ips2:
-                interf2 = interface(next,f'node{i}',[],[])
-                neighbors.append(interf2)
-                i = i+1
-            graph[interf1] = neighbors
-        return graph
+                interf2 = interface(next,[],[])
+                if not any(obj.ip == next for obj in nodes):
+                    nodes.append(interf2)
+                neighbors_aux.append((interf1,interf2))
 
-    # grapf = constructGraph('oNode [10.0.0.1 : 10.0.0.2 , 10.0.0.3] [10.0.0.2 : 10.0.0.3, 10.0.0.6]')
+        neighbors = []
+        for n in neighbors_aux:
+            neighbors.append((getIndex(nodes,n[0].ip),getIndex(nodes,n[1].ip),1))
+
+        graph = Graph(len(nodes),neighbors)
+
+        return nodes,graph
+
+#                                     0          1          2           1         2           3
+    grapf = constructGraph('oNode [10.0.0.1 : 10.0.0.2 , 10.0.0.3] [10.0.0.2 : 10.0.0.3, 10.0.0.6]')
+    
