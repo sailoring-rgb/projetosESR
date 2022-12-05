e62f85eed88dfd2a2ddc0aa4235d173d7b0d3d7c
Francisco Peixoto <thtr.irubra@gmail.com>
2022-12-05 13:50:58 +0000

    formating files

--- TP2/src/teste/flooding.py
+++ TP2/src/teste/flooding.py
@@ -1,32 +1,34 @@
 from collections import deque
 
+
 class Graph:
-  def __init__(self):
-    self.nodes = set()
-    self.edges = dict()
+    def __init__(self):
+        self.nodes = set()
+        self.edges = dict()
 
-  def add_node(self, node):
-    self.nodes.add(node)
+    def add_node(self, node):
+        self.nodes.add(node)
 
-  def add_edge(self, node1, node2):
-    if node1 not in self.edges:
-      self.edges[node1] = set()
-    self.edges[node1].add(node2)
+    def add_edge(self, node1, node2):
+        if node1 not in self.edges:
+            self.edges[node1] = set()
+        self.edges[node1].add(node2)
 
-  def flood(self, source, message):
-    visited = set()
-    queue = deque([source])
+    def flood(self, source, message):
+        visited = set()
+        queue = deque([source])
 
-    while queue:
-      current = queue.popleft()
-      visited.add(current)
+        while queue:
+            current = queue.popleft()
+            visited.add(current)
 
-      print('Sending message to %s: %s' % (current, message))
+            print('Sending message to %s: %s' % (current, message))
 
-      if current in self.edges:
-        for neighbor in self.edges[current]:
-          if neighbor not in visited:
-            queue.append(neighbor)
+            if current in self.edges:
+                for neighbor in self.edges[current]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
 
 # create a new graph
 g = Graph()
