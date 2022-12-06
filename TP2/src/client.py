d65b4dbb95a3f46ccbfc4100a3b2f90e5d6c3fbd
Ana Henriques <62114404+AnaHenriques01@users.noreply.github.com>
2022-12-06 20:19:08 +0000

    update

--- TP2/src/client.py
+++ TP2/src/client.py
@@ -1,29 +1,32 @@
-import sys
+import sys, re, os, socket
 from tkinter import Tk
-from ClientWorker import ClientWorker
-import os
-import re
+from ClientStreamer import ClientStreamer
 
-if __name__ == "__main__":
+
+def streamVideo(serverAddr: str, serverPort: int, rtpAddress: str, rtpPort: int):
 	current_pwd_path = os.path.dirname(os.path.abspath(__file__))
 	video_pwd_path = (re.findall("(?:(.*?)src)",current_pwd_path))[0]
-	try:
-		serverAddr = sys.argv[1]
-		serverPort = sys.argv[2]
-		rtpAddress = sys.argv[3]
-		rtpPort = sys.argv[4]
-		fileName = os.path.join(video_pwd_path,"video/"+str(sys.argv[5]))
-	except:
-		print("[Usage: ClientLauncher.py Server_Addr Server_Port RTP_Port Video_file_Name]\n")	
-	
-	if os.environ.get('DISPLAY','') == '':
-		print('No display found... Using DISPLAY :0.0')
-		os.environ.__setitem__('DISPLAY', ':0.0')
+	filename = input(f'Introduza o nome do video: \n')
+	path_to_filename = os.path.join(video_pwd_path,"play/"+str(filename))
 
 	root = Tk()
-	
 	# Create a new client
-	app = ClientWorker(root, serverAddr, serverPort, rtpAddress, rtpPort, fileName)
-	app.master.title("RTPClient")
-
+	app = ClientStreamer(root, serverAddr, serverPort, rtpAddress, rtpPort, path_to_filename)
+	app.master.title("RTP Client")
 	root.mainloop()
+
+
+if __name__ == "__main__":
+	try:
+		serverAddr = sys.argv[1]  # server address
+		serverPort = sys.argv[2]  # server port
+		rtpAddress = sys.argv[3]  # client address
+		rtpPort = sys.argv[4]     # client port
+	except:
+		print("[Usage: ClientLauncher.py Type_Stream Server_Addr Server_Port RTP_Port]\n")	
+	
+	if os.environ.get('DISPLAY','') == '':
+		print('No display found... Using DISPLAY :0.0\n')
+		os.environ.__setitem__('DISPLAY', ':0.0')
+
+	streamVideo(serverAddr,serverPort,rtpAddress,rtpPort)
